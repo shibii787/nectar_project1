6484740f70d2de44432321812f118897fd758fe0
USER <shibilmuhammed787@gmail.com>
2024-03-28 14:23:40 +0530

    logo

---
+++ assets/logos/logo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class logo extends StatefulWidget {
+  const logo({super.key});
+
+  @override
+  State<logo> createState() => _logoState();
+}
+
+class _logoState extends State<logo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
