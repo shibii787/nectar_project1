b174a43649f6a21cda86d3d3888d334f7f8f1e30
User <shamilmohammed986@gmail.com>
2024-04-01 14:38:41 +0530

    update login page

--- lib/images.dart
+++ lib/images.dart
@@ -1,13 +1,12 @@
-import 'package:nectar_project1/onboarding.dart';
-
 class theImages{
-static const orangeCarrot="assets/images/Group (1).png";
-static const whiteCarrot="assets/images/Group.png";
-static const onboarding="assets/images/8140 1.png"; //onboarding background image
-static const Redbell="assets/images/Red Bell.png"; //
-static const gooleimage="assets/images/Group 6795.png"; //
-static const fbimage="assets/images/Vector.png"; //
-static const signimage="assets/images/Mask Group.png";
-static const onboarding1="assets/images/8140 1.png";//sign image
-static const whitecarrotsvg="assets/images/white carrot svg.svg";//sign image
+  static const orangeCarrot="assets/images/Group (1).png";
+  static const whiteCarrot="assets/images/Group.png";
+  static const onboarding="assets/images/8140 1.png"; //onboarding background image
+  static const Redbell="assets/images/Red Bell.png"; //
+  static const googleimage="assets/images/Group 6795.png"; //
+  static const fbimage="assets/images/Vector.png"; //
+  static const signimage="assets/images/Mask Group.png";
+  static const sucess="assets/icons/Animation - 1711958595830.json";
+  static const backgroundpayment="assets/images/order acc back.png";
+  static const whitecarrotsvg="";//sign image
 }
