8bb6e16f0ad8add0b3b2aafeb428b262e7fe57d5
USER <shibilmuhammed787@gmail.com>
2024-04-03 15:16:03 +0530

    navigation done

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
             textTheme: GoogleFonts.poppinsTextTheme()
         ),
         debugShowCheckedModeBanner: false,
-        home: splashscreen(),
+        home: bottomNav(),
       ),
     );
   }
