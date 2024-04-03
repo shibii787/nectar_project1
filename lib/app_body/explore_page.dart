7898397e8182c6a9353da141b9390016d99500f8
mohammedubaise <ubaiseap35@gmail.com>
2024-04-03 13:37:17 +0530

    explorepage

---
+++ lib/app_body/explore_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nectar_project1/colors.dart';
+import 'package:nectar_project1/images.dart';
+
+import '../main.dart';
+
+class ExplorePage extends StatefulWidget {
+  const ExplorePage({super.key});
+
+  @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  // Gridview Datas is in the List of Map
+  List datas=[
+    {
+      "image":theImages.fruitsAndveg,
+      "title":"Fresh Fruits",
+      "subtitle":"& Vegetables"
+    },
+    {
+      "image":theImages.oil,
+      "title":"Cooking Oil",
+      "subtitle":"& Ghee"
+    },
+    {
+      "image":theImages.meatAndfish,
+      "title":"Meat & Fish",
+      "subtitle":""
+
+
+    },
+    {
+      "image":theImages.bakeryAndSnacks,
+      "title":"Bakery & Snacks",
+      "subtitle":""
+
+    },
+    {
+      "image":theImages.dairyAndEggs,
+      "title":"Dairy & Eggs",
+      "subtitle":""
+
+    },
+    {
+      "image":theImages.beverages,
+      "title":"Beverages",
+      "subtitle":""
+
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theColors.primaryColor,
+        title: Text("Find Products",style: TextStyle(
+          fontWeight: FontWeight.w900,),),
+
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding:  EdgeInsets.all(w*0.03),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(w*0.03) ,
+                  borderSide: BorderSide(color: theColors.sixth)
+                ),
+                prefixIcon: Icon(Icons.search),
+                hintText: "Search Store"
+              ),
+            ),
+            SizedBox(height: w*0.05,),
+            Expanded(
+              child: GridView.builder(
+                itemCount: datas.length,
+
+                physics: BouncingScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: w*0.03,
+                  mainAxisSpacing: w*0.06,
+                  childAspectRatio:0.8 ,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return  Column(
+                    children: [
+                      Container(
+                        width: w*0.5,
+                        height: w*0.55,
+                        decoration: BoxDecoration(
+                            color: theColors.third.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(w*0.05),
+                          border: Border.all(width: w*0.002,color: theColors.third)
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: w*0.3,
+                                height: w*0.2,
+
+                                child: Image(image:  AssetImage(datas[index]["image"]),fit: BoxFit.fill)
+                            ),
+                            Column(
+                              children: [
+                                Text(datas[index]["title"],textAlign: TextAlign.center,
+                                  style:TextStyle(fontSize: w*0.04,
+                                  fontWeight: FontWeight.w900) ,),
+
+                            Text(datas[index]["subtitle"],textAlign: TextAlign.center,
+                              style:TextStyle(fontSize: w*0.04,
+                                  fontWeight: FontWeight.w900) ,)
+                              ],
+                            ),
+                          ],
+
+                        ),
+
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nectar_project1/app_authentication/log_in.dart';
-import 'package:nectar_project1/app_authentication/sign_page.dart';
-import 'package:nectar_project1/app_authentication/signup_page.dart';
-import 'package:nectar_project1/app_authentication/verification_page.dart';
-import 'package:nectar_project1/app_body/cart_page.dart';
-import 'package:nectar_project1/app_body/order_accepted.dart';
-import 'package:nectar_project1/app_into/splashscreen.dart';
+import 'package:nectar_project1/app_body/explore_page.dart';
+
 import 'package:nectar_project1/app_profile/account.dart';
 import 'app_body/bottom_nav.dart';
 
@@ -43,7 +40,7 @@ class MyApp extends StatelessWidget {
             textTheme: GoogleFonts.poppinsTextTheme()
         ),
         debugShowCheckedModeBanner: false,
-        home: bottomNav(),
+        home: ExplorePage(),
       ),
     );
   }
